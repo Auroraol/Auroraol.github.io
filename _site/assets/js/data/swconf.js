b84db97c0a393a134cf633d3917203ad190b9966
liufengjie <1665834268@qq.com>
2025-11-02 21:40:09 +0800

    0

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1762090699',resources: [
+    cacheName: 'chirpy-1762090802',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
