9b7d09178fd30a21519f079d678d307c4e3d47cf
liufengjie <98401567+Auroraol@users.noreply.github.com>
2025-11-23 00:54:49 +0800

    3

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763826634',resources: [
+    cacheName: 'chirpy-1763830416',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
